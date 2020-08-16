24dd738c7bd3f495dfb2cba53455a0fadad72235
cmukrammer <chenghul@alumni.cmu.edu>
2020-08-16 10:18:15 -0700

    Init masonry after image load...

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 // element argument can be a selector string
 //   for an individual element
-var msnry = new Masonry( '.grid', {
-    itemSelector: '.grid-item',
-    // use element for option
-    columnWidth: '.grid-sizer',
-    gutter: 10,
-    percentPosition: true,
-    resize: true
-});
+var msnry;
 
-imagesLoaded(msnry, function() {
-    msnry.layout();
+imagesLoaded(document.querySelectorAll('.grid'), function() {
+    msnry = new Masonry( '.grid', {
+        itemSelector: '.grid-item',
+        // use element for option
+        columnWidth: '.grid-sizer',
+        gutter: 10,
+        percentPosition: true,
+        resize: true
+    });
 })
